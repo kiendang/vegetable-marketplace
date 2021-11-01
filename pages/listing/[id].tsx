164f1d9ba69015d60f20bc910b0541e9116bf501
Kien Dang <mail@kien.ai>
2021-11-02 03:31:40 +0800

    Add order confirmation modal

--- pages/listing/[id].tsx
+++ pages/listing/[id].tsx
@@ -1,11 +1,13 @@
 import { Button, Typography } from '@mui/material'
 import { Box } from '@mui/system'
+import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material'
 import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
 import { useSnackbar } from 'notistack'
 import ErrorHanlder from '../../components/common/ErrorHandler'
 import Loader from '../../components/common/Loader'
 import { useCreateOrder, useListing } from '../../lib/frontend/data'
+import { useState } from 'react'
 
 const Dashboard: NextPage = () => {
   const router = useRouter()
@@ -13,11 +15,13 @@ const Dashboard: NextPage = () => {
   const { createOrder } = useCreateOrder()
   const { enqueueSnackbar } = useSnackbar()
   const { listing, isLoading, isError } = useListing(id)
+
+  const [open, setOpen] = useState(false)
+
   if (isError) return <ErrorHanlder error={isError} />
   if (!listing || isLoading) return <Loader />
 
-  const submitData = async (e: React.SyntheticEvent) => {
-    e.preventDefault()
+  const submitData = async () => {
     try {
       await createOrder(id)
       enqueueSnackbar('Order successfully placed!', { variant: 'success' })
@@ -26,19 +30,50 @@ const Dashboard: NextPage = () => {
     }
   }
 
+  const handleClickOpen = () => {
+    setOpen(true)
+  }
+
+  const handleClose = () => {
+    setOpen(false)
+  }
+
+  const handleConfirm = async (e: React.SyntheticEvent) => {
+    e.preventDefault()
+    await submitData()
+    handleClose()
+  }
+
   return (
     <Box m={4}>
       <Typography variant="h3">{listing.title}</Typography>
       <Typography variant="body1">{listing.description}</Typography>
-      <form onSubmit={submitData}>
-        <Box my={2}>
-          <Box mr={2}>
-            <Button variant="contained" type="submit">
-              Place order
-            </Button>
-          </Box>
+      <Box my={2}>
+        <Box mr={2}>
+          <Button variant="contained" onClick={handleClickOpen}>
+            Place order
+          </Button>
+          <Dialog
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+            <DialogTitle id="alert-dialog-title">
+              Order confirmation
+            </DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                {`Would you like to place an order for ${listing.title}?`}
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose} autoFocus>No</Button>
+              <Button onClick={handleConfirm}>Yes</Button>
+            </DialogActions>
+          </Dialog>
         </Box>
-      </form>
+      </Box>
     </Box>
   )
 }
